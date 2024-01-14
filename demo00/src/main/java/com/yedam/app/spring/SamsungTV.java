6cad9887f12444ba2fafbc38e396c39bc0784f04
KangHyunJin2 <khj0501k@naver.com>
2024-01-14 20:40:28 +0900

    커밋

--- demo00/src/main/java/com/yedam/app/spring/SamsungTV.java
+++ demo00/src/main/java/com/yedam/app/spring/SamsungTV.java
@@ -3,15 +3,31 @@ package com.yedam.app.spring;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
+//(TV)ctx.getBean(TV.class); 이 방식이면 component이름 안붙여도 됨
+@Component("tv") 
 public class SamsungTV implements TV {
-	@Autowired
-	private Speaker speaker = null;
 	
+	@Autowired	
+	Speaker speaker;
+	
+	//생성자방식 > Autowired
+	SamsungTV(Speaker speaker){
+		this.speaker = speaker;
+	}
+	
+	
+	//setter방식
+	SamsungTV(){}
+	/* 만약  SamsungTV(Speaker speaker){ this.speaker = speaker; }
+	 * 이것만 있으면 기본 생성자가 생성 안됨 > setter 사용 안됨  */
+	
+	public void setSpeaker(Speaker speaker) {
+		this.speaker = speaker;
+	}
+
 	@Override
 	public void on() {
-		// TODO Auto-generated method stub
-		System.out.println("삼성 TV를 킴");
+		System.out.println("삼성 TV를켰습니다!");
 		speaker.on();
 	}
 }
--- demo00/src/main/java/com/yedam/app/spring/SpringMainClass.java
+++ demo00/src/main/java/com/yedam/app/spring/SpringMainClass.java
@@ -9,6 +9,7 @@ public class SpringMainClass {
 		ApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationContext.xml");
 		
 		TV tv = (TV)ctx.getBean(SamsungTV.class);
+		//TV tv = (TV)ctx.getBean(TV.class); 사용하고자하는 클래스나 인터페이스 기반으로 등록되어있는 인터페이스 불러오는 방식
 		tv.on();
 	}
 }
